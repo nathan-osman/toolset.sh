33e10f5ae349f5faa149ccad1b2bc6c9fb510134
Nathan Osman <nathan@quickmediasolutions.com>
2025-06-01 22:07:13 -0700

    Add template and parameter utility methods.

---
+++ util/params.go
@@ -0,0 +1,27 @@
+package util
+
+import "strconv"
+
+// GetStringParam returns the specified parameter if provided or the default
+// if not.
+func GetStringParam(params map[string]string, name, def string) string {
+	v, ok := params[name]
+	if !ok {
+		return def
+	}
+	return v
+}
+
+// GetFloatParam returns the specified parameter as a float if provided or the
+// default if not.
+func GetFloatParam(params map[string]string, name string, def float64) float64 {
+	v, ok := params[name]
+	if !ok {
+		return def
+	}
+	f, err := strconv.ParseFloat(v, 64)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
